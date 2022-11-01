44906fc8430d83952f93a7a64c1219e10e2341f4
Michael attah <immichaelchampionss@gmail.com>
2022-11-01 08:07:05 -0700

    console filemodified

--- console.py
+++ console.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 """ Module for the entry point of the command interpreter."""
+
+
 import cmd
 from models.base_model import BaseModel
 from models import storage
 import re
 import json
-
 
 class HBNBCommand(cmd.Cmd):
 
